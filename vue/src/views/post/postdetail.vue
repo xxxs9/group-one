eb6654993e169712ef57776e40e29c2e9b37f6fb
llh <1174626464@qq.com>
2019-06-23 16:54:05 +0800

    postDetails update1

--- vue/src/views/post/postdetail.vue
+++ vue/src/views/post/postdetail.vue
@@ -7,15 +7,48 @@
           用户信息
         </el-header>
         <el-main>
-          {{addDetailData}}
+          {{addDetailData.postText}}
         </el-main>
-        {{addDetailData}}
+        <el-footer>
+          <span>
+            电话：{{addDetailData.postPhone}}
+          </span>
+          <br/>
+          <span>
+            地址：{{addDetailData.postAddress}}
+          </span>
+        </el-footer>
+        <el-main>
+          <span>
+            <img :src="addDetailData.postImgList[0]" style="width: 130px;height: 130px"/>
+            <img :src="addDetailData.postImgList[1]" style="width: 130px;height: 130px"/>
+            <img :src="addDetailData.postImgList[2]" style="width: 130px;height: 130px"/>
+          </span>
+          <br/>
+          <span>
+            <img :src="addDetailData.postImgList[3]" style="width: 130px;height: 130px"/>
+            <img :src="addDetailData.postImgList[4]" style="width: 130px;height: 130px"/>
+            <img :src="addDetailData.postImgList[5]" style="width: 130px;height: 130px"/>
+          </span>
+          <br/>
+          <span>
+            <img :src="addDetailData.postImgList[6]" style="width: 130px;height: 130px"/>
+            <img :src="addDetailData.postImgList[7]" style="width: 130px;height: 130px;border: none!important;"/>
+            <img :src="addDetailData.postImgList[8]" style="width: 130px;height: 130px"/>
+          </span>
+        </el-main>
+        <el-footer>
+          <span>
+            <el-tag v-for="tag in addDetailData.postTagList" v-text="tag" />
+          </span>
+        </el-footer>
+        <el-header>
+          <el-button type="text">评论</el-button>
+        </el-header>
+        <el-main v-for="text in addDetailData.commentText">
+          <span>评论用户 回复 被评论用户： <el-tag v-text="text" /></span>
+        </el-main>
       </span>
-      <el-button @click="getDetailData">显示</el-button>
-
-
-
-
   </div>
 </template>
 
@@ -45,5 +78,21 @@
 
 
 <style scoped>
+  .el-header {
+    color: #333;
+    text-align: left;
+    line-height: 20px;
+  }
 
+  .el-footer {
+    color: #333;
+    text-align: left;
+    line-height: 20px;
+  }
+
+  .el-main {
+    color: #333;
+    text-align: left;
+    line-height: 20px;
+  }
 </style>
