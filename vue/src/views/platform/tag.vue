2251e45899790054a4b3865d0373176cb4db4584
enemy <1578537966@qq.com>
2019-06-26 09:16:26 +0800

    day 8

--- vue/src/views/platform/tag.vue
+++ vue/src/views/platform/tag.vue
@@ -20,6 +20,8 @@
       <el-form-item>
         <el-button type="primary" icon="plus" @click="showCreate" v-if="hasPerm('article:add')">添加
         </el-button>
+        <el-button type="primary" icon="plus" @click="showCreate" v-if="hasPerm('article:add')">批量添加
+        </el-button>
         <el-button @click="getData">获取数据</el-button>
         <el-button type="danger" icon="delete" @click="removeUser()">删除
         </el-button>
@@ -32,8 +34,8 @@
           <span v-text="getIndex(scope.$index)"> </span>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="类别名称" prop="name" style="width: 60px;"></el-table-column>
-      <el-table-column align="center" label="上级名称"  prop="parentId" width="100">
+      <el-table-column align="center" label="标签名称" prop="name" style="width: 60px;"></el-table-column>
+      <el-table-column align="center" label="上级标签"  prop="parentId" width="100">
         <template slot-scope="scope">
           <el-tag  v-if="scope.row.rank==1" style="margin-right: 3px;" type="success">无</el-tag>
           <div v-if="scope.row.rank!=1" v-for="tags in list" style="text-align: center">
@@ -42,13 +44,52 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column align="center" label="等级" prop="rank" width="170"></el-table-column>
+      <!--<el-table-column align="center" label="等级" prop="rank" width="170"></el-table-column>-->
       <el-table-column align="center" label="管理" width="220">
         <template slot-scope="scope">
           <el-button type="primary" icon="edit" @click="showUpdate(scope.$index)">修改</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="listQuery.pageNum"
+      :page-size="listQuery.pageRow"
+      :total="totalCount"
+      :page-sizes="[10, 20, 50, 100]"
+      layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+
+      <el-form class="small-space" :model="tempTag" label-position="left" label-width="80px"
+               style='width: 300px; margin-left:50px;'>
+        <el-form-item label="上级标签 " required>
+          <el-select v-model="tempTag.parentId" placeholder="请选择" >
+            <el-option
+            key="0"
+            label="无"
+            value="0"
+            ></el-option>
+            <el-option
+              v-for="item in typeOption"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="标签名称" required>
+          <el-input type="text" v-model="tempTag.tname">
+          </el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button v-if="dialogStatus=='create'" type="success" @click="createUser">创 建</el-button>
+        <el-button type="primary" v-else @click="updateUser">修 改</el-button>
+      </div>
+    </el-dialog>
   </div>
 
 
@@ -64,6 +105,7 @@
           children: 'children',
           label: 'name'
         },
+        typeOption: '',
         totalCount: 0, //分页组件--数据总条数
         list: [],//表格的数据
         listLoading: false,//数据加载等待动画
@@ -81,10 +123,10 @@
         tempID:{
           id:""
         },
-        tempArticle: {
-          tid: "",
-          tname: "",
-          pid: ""
+        tempTag: {
+          id: "",
+          name: "",
+          parentId: ""
         }
       }
     },
@@ -116,6 +158,7 @@
         }).then(data => {
           this.listLoading = false;
           this.data = data.list;
+          this.typeOption = data.list;
         })
       },
       getList() {
@@ -131,6 +174,7 @@
         }).then(data => {
           this.listLoading = false;
           this.list = data.list;
+          this.totalCount = data.totalCount;
         })
       },
       getIndex($index) {
@@ -157,16 +201,51 @@
       },
       showCreate() {
         //显示新增对话框
-        this.tempArticle.content = "";
+         this.tempTag.parentId = "";
+        this.tempTag.content = "";
         this.dialogStatus = "create"
         this.dialogFormVisible = true
       },
       showUpdate($index) {
         //显示修改对话框
-        this.tempArticle.id = this.list[$index].id;
-        this.tempArticle.content = this.list[$index].content;
+        this.tempTag.id = this.list[$index].id;
+        this.tempTag.content = this.list[$index].content;
         this.dialogStatus = "update"
         this.dialogFormVisible = true
+      },
+      createUser() {
+        //添加新用户
+        this.api({
+          url: "/sort/addSort",
+          method: "post",
+          data: this.tempUser
+        }).then(() => {
+          this.getList();
+          this.dialogFormVisible = false
+        })
+      },
+      updateUser() {
+        //修改用户信息
+        let _vue = this;
+        this.api({
+          url: "/sort/updateSort",
+          method: "post",
+          data: this.tempUser
+        }).then(() => {
+          let msg = "修改成功";
+          this.dialogFormVisible = false
+          if (this.userId === this.tempUser.userId) {
+            msg = '修改成功,部分信息重新登录后生效'
+          }
+          this.$message({
+            message: msg,
+            type: 'success',
+            duration: 1 * 1000,
+            onClose: () => {
+              _vue.getList();
+            }
+          })
+        })
       },
       removeUser() {
         this.$confirm('确定删除此用户?', '提示', {
--- vue/src/views/template/chat.vue
+++ vue/src/views/template/chat.vue
@@ -25,6 +25,17 @@
        <el-button @click="getData">获取数据</el-button>
        <el-button type="danger" icon="delete" @click="removeUser()">删除
        </el-button>
+       <Upload
+         ref="upload"
+         action="此处填写你访问的后台接口地址"
+         :on-format-error="handleFormatError"
+         :on-success="handleSuccess"
+         :on-error="handleError"
+         enctype="multipart/form-data"
+         :format ="['xlsx','xls']">
+         <Button type="primary" icon="ios-cloud-upload-outline">批量导入</Button>
+       </Upload>
+
      </el-form-item>
    </el-form>
    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
@@ -50,114 +61,67 @@
 
 <script>
   export default {
-    name: "Test",
-    data(){
+    data() {
       return {
-        data : [],
-        defaultProps: {
-          children: 'children',
-          label: 'name'
+        uploadBtnIcon: 'el-icon-upload2',
+        enabledUploadBtn: true,
+        dialogVisible: false,
+        btnText: '数据导入',
+        jl: {
+          name: '',
+          titlelevel: '中级'
         },
-        totalCount: 0, //分页组件--数据总条数
-        list: [],//表格的数据
-        listLoading: false,//数据加载等待动画
-        listQuery: {
-          pageNum: 1,//页码
-          pageRow: 50,//每页条数
-          name: ''
-        },
-        dialogStatus: 'create',
-        dialogFormVisible: false,
-        textMap: {
-          update: '编辑',
-          create: '创建文章'
-        },
-        tempID:{
-          id:""
-        },
-        tempArticle: {
-          tid: "",
-          tname: "",
-          pid: ""
-        }
+        jls: [],
+        tls: [{
+          value: '员级',
+          label: '员级'
+        }, {
+          value: '初级',
+          label: '初级'
+        }, {
+          value: '中级',
+          label: '中级'
+        }, {
+          value: '副高级',
+          label: '副高级'
+        }, {
+          value: '正高级',
+          label: '正高级'
+        }],
+        value: ''
       }
     },
-    created() {
-      this.getList();
+    mounted() {
+      this.initJls();
     },
-    computed:{
-      treeData(){
-        let cloneData = JSON.parse(JSON.stringify(this.data))    // 对源数据深度克隆
-        return cloneData.filter(father=>{
-          let branchArr = cloneData.filter(child=>father.id == child.parentId)    //返回每一项的子级数组
-          branchArr.length>0 ? father.children = branchArr : ''   //如果存在子级，则给父级添加一个children属性，并赋值
-          return father.parentId==0;      //返回第一层
-        });
-      }
-    },
-    methods:{
-      getList() {
-        //查询列表
-        if (!this.hasPerm('article:list')) {
-          return
-        }
-        this.listLoading = true;
-        this.api({
-          url: "/tag/listTag",
-          method: "get",
-          params: this.listQuery
-        }).then(data => {
-          this.listLoading = false;
-          this.data = data.list;
-          this.list = data.list;
+    methods: {
+
+      onSuccess(response, file, fileList) {
+        this.enabledUploadBtn = true;
+        this.uploadBtnIcon = "el-icon-upload2";
+        this.btnText = '数据导入';
+        alert("数据导入成功！");
+        this.initJls();
+      },
+
+      onError(err, file, fileList) {
+        this.enabledUploadBtn = true;
+        this.uploadBtnIcon = "el-icon-upload2";
+        this.btnText = '数据导入';
+        alert("数据导入失败！请检查是否有重复数据,和网络连接状况！");
+      },
+      beforeUpload(file) {
+        this.enabledUploadBtn = false;
+        this.uploadBtnIcon = "el-icon-loading";
+        this.btnText = '正在导入';
+      },
+      initJls() {
+        this.getRequest("/system/basic/jl/").then(resp => {
+          if (resp) {
+            this.jls = resp;
+          }
         })
-      },
-      getData(){
-        alert(this.$refs.tree.getCheckedKeys().concat(this.$refs.tree.getHalfCheckedKeys()));
-
-      },
-      handleNodeClick(data){
-        // console.log(data)
-        console.log(this.treeData)
-      },
-      showCreate() {
-        //显示新增对话框
-        this.tempArticle.content = "";
-        this.dialogStatus = "create"
-        this.dialogFormVisible = true
-      },
-      showUpdate($index) {
-        //显示修改对话框
-        this.tempArticle.id = this.list[$index].id;
-        this.tempArticle.content = this.list[$index].content;
-        this.dialogStatus = "update"
-        this.dialogFormVisible = true
-      },
-      removeUser() {
-         this.$confirm('确定删除此用户?', '提示', {
-           confirmButtonText: '确定',
-           showCancelButton: false,
-           type: 'warning'
-         }).then(() => {
-            let temp = this.$refs.tree.getCheckedKeys().concat(this.$refs.tree.getHalfCheckedKeys());
-            alert(temp);
-
-            this.tempID.id = temp;
-
-
-           this.api({
-             url: "/tag/deleteTag",
-             method: "post",
-             data: this.tempID,
-         }).then(() => {
-             this.getList()
-           }).catch(() => {
-             this.$message.error("删除失败")
-           })
-         })
-      },
-    },
-
-
+      }
+    }
   }
 </script>
