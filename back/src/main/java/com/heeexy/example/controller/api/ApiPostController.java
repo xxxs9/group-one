6440179f7dda125b1d46d7487eba061184ebc60a
hzj <100995904@qq.com>
2019-07-01 12:26:19 +0800

    hzj day14 commit3

--- back/src/main/java/com/heeexy/example/controller/api/ApiPostController.java
+++ back/src/main/java/com/heeexy/example/controller/api/ApiPostController.java
@@ -36,6 +36,6 @@ public class ApiPostController {
     @PostMapping("/listPostType")
     public JSONObject listPostType(@RequestBody JSONObject requestJson) {
         CommonUtil.hasAllRequired(requestJson, "postStick");
-        return postService.listPostApi(requestJson);
+        return postService.getPostListApi(requestJson);
     }
 }
